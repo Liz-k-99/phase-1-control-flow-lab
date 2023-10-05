2d7334c1073eb88823d4c64b180eb5a6ba827b42
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-10-05 12:39:12 +1100

    if + ternary statment added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rides){
   // Write your code here!
+  
+  if (rides <= 400 ){
+    return "This one is on me!";
+  }else if (rides <= 2000){
+    return "That will be twenty bucks."
+  }else if (rides <= 2500){
+    return"I will gladly take your thirty bucks."
+  }else{return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go.";
 }
 
 function switchOnCharmFromTip(){
